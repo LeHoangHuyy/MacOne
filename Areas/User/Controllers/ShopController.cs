459004e5d5b5045261f789a8f27ed01a1816f419
ABC <perconmy3n@gmail.com>
2025-10-18 17:28:09 +0700

    #12 Them ProductApiController + Ajax va chinh sua Route cho Area User

--- Areas/User/Controllers/ShopController.cs
+++ Areas/User/Controllers/ShopController.cs
@@ -7,34 +7,12 @@ using X.PagedList;
 namespace Macone.Areas.User.Controllers
 {
     [Area("User")]
+    [Route("User/Shop")]
     public class ShopController : Controller
     {
-        private readonly AppDbContext _db;
-
-        public ShopController(AppDbContext db)
+        public IActionResult Index()
         {
-            _db = db;
-        }
-
-        public IActionResult Index(int? id, int? page)
-        {
-            int pageSize = 9;
-            int pageNumber = page == null || page <= 0 ? 1 : page.Value;
-
-            var query = _db.Products.Include(p => p.Images).AsNoTracking();
-
-            if (id != null)
-            {
-                query = query.Where(x => x.CategoryId == id);
-            }
-
-            var products = query.OrderByDescending(x => x.CreatedAt);
-
-            PagedList<Product> lst = new PagedList<Product>(products, pageNumber, pageSize);
-
-            ViewData["Title"] = "Cửa hàng";
-
-            return View(lst);
+            return View();
         }
     }
 }
